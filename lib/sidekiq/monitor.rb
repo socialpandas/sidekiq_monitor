2035dac624ea4b12be7f652cdb442ea791e0e054
Tom Benner <tombenner@gmail.com>
2013-04-10 11:54:05 -0700

    No need to require automatically required files

--- lib/sidekiq/monitor.rb
+++ lib/sidekiq/monitor.rb
@@ -2,10 +2,6 @@ require 'slim'
 
 directory = File.dirname(File.absolute_path(__FILE__))
 Dir.glob("#{directory}/monitor/**/*.rb") { |file| require file }
-Dir.glob("#{directory}/../../app/datatables/*.rb") { |file| file; require file }
-Dir.glob("#{directory}/../../app/helpers/sidekiq/monitor/*.rb") { |file| file; require file }
-Dir.glob("#{directory}/../../app/controllers/sidekiq/monitor/*.rb") { |file| file; require file }
-Dir.glob("#{directory}/../../app/models/sidekiq/monitor/*.rb") { |file| file; require file }
 
 module Sidekiq
   module Monitor
