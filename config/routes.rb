4d1c0da4cad3f9898c8134320ad455be697e871d
Jose Irizarry <protechpr@gmail.com>
2013-07-12 11:12:18 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Sidekiq::Monitor::Engine.routes.draw do
-  get '/' to: 'jobs#index', :as => 'sidekiq_monitor'
-  get '/queues' to: 'queues#index'
+  get '/', to: 'jobs#index', :as => 'sidekiq_monitor'
+  get '/queues', to: 'queues#index'
   
   namespace 'api' do
-    get '/jobs' to: 'jobs#index'
+    get '/jobs', to: 'jobs#index'
     match '/jobs/:action(/:id)' => 'jobs', via: :all
-    get '/queues/:queue' to: 'queues#show'
+    get '/queues/:queue', to: 'queues#show'
   end
 end
