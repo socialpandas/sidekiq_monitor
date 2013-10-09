277b0cf7141d1d2890fa86e3a03bbb1ba73a83d2
Tom Benner <tombenner@gmail.com>
2013-10-09 11:46:00 -0700

    Clean up Processor; catch errors thrown in job_name

--- lib/sidekiq/monitor/processor.rb
+++ lib/sidekiq/monitor/processor.rb
@@ -2,60 +2,25 @@ module Sidekiq
   module Monitor
     class Processor
       def queue(worker_class, item, queue)
-        args = item['args']
-        name = job_name(worker_class, args)
-        Sidekiq::Monitor::Job.find_or_create_by_jid(
-          jid: item['jid'],
-          queue: queue,
-          class_name: worker_class.is_a?(String) ? worker_class : worker_class.name,
-          args: args,
-          retry: item['retry'],
-          enqueued_at: DateTime.now,
-          status: 'queued',
-          name: name
-        )
+        job = find_or_initialize_job(worker_class, item, queue)
+        job.save
       end
 
-      def start(worker, msg, queue)
-        jid = msg['jid']
-        args = msg['args']
-        now = DateTime.now
-        job = Sidekiq::Monitor::Job.find_by_jid(jid)
-        if job.blank?
-          name = job_name(worker.class, args)
-          job = Sidekiq::Monitor::Job.new(
-            jid: jid,
-            queue: queue,
-            class_name: worker.class.name,
-            args: args,
-            retry: msg['retry'],
-            enqueued_at: now,
-            name: name
-          )
-        end
+      def start(worker, item, queue)
+        job = find_or_initialize_job(worker, item, queue)
         job.update_attributes(
-          started_at: now,
+          started_at: DateTime.now,
           status: 'running'
         )
       end
 
-      def error(worker, msg, queue, exception)
-        result = {
-          message: "#{exception.class.name}: #{exception.message}",
-          backtrace: exception.backtrace
-        }
-        job = find_job(msg)
-        return unless job
-        job.update_attributes(
-          finished_at: DateTime.now,
-          status: 'failed',
-          result: result
-        )
+      def error(worker, item, queue, exception)
+        job = find_or_initialize_job(worker, item, queue)
+        set_error(job, exception)
       end
 
-      def complete(worker, msg, queue, return_value)
-        job = find_job(msg)
-        return unless job
+      def complete(worker, item, queue, return_value)
+        job = find_or_initialize_job(worker, item, queue)
         job.update_attributes(
           finished_at: DateTime.now,
           status: 'complete',
@@ -65,12 +30,62 @@ module Sidekiq
 
       protected
 
-      def find_job(msg)
-        Sidekiq::Monitor::Job.find_by_jid(msg['jid'])
+      def find_or_initialize_job(worker, item, queue, options={})
+        defaults = {
+          set_name: true
+        }
+        options.reverse_merge!(defaults)
+
+        worker_class = nil
+        if worker.is_a?(String)
+          worker_class = worker.constantize
+        elsif worker.is_a?(Class)
+          worker_class = worker
+        else
+          worker_class = worker.class
+        end
+
+        job = Sidekiq::Monitor::Job.find_by_jid(item['jid'])
+        if job.blank?
+          attributes = {
+            jid: item['jid'],
+            queue: queue,
+            class_name: worker_class.name,
+            args: item['args'],
+            retry: item['retry'],
+            enqueued_at: DateTime.now,
+            status: 'queued'
+          }
+          if options[:set_name] == true
+            attributes[:name] = job_name(worker_class, item, queue)
+          end
+          job = Sidekiq::Monitor::Job.new(attributes)
+        end
+        job
       end
 
-      def job_name(worker_class, args)
-        worker_class.respond_to?(:job_name) ? worker_class.job_name(*args) : nil
+      def job_name(worker_class, item, queue)
+        args = item['args']
+        begin
+          worker_class.respond_to?(:job_name) ? worker_class.job_name(*args) : nil
+        rescue Exception => exception
+          # If the job doesn't exist yet, we'll need to create it
+          job = find_or_initialize_job(worker_class, item, queue, set_name: false)
+          set_error(job, exception)
+          raise exception
+        end
+      end
+
+      def set_error(job, exception)
+        result = {
+          message: "#{exception.class.name}: #{exception.message}",
+          backtrace: exception.backtrace
+        }
+        job.update_attributes(
+          finished_at: DateTime.now,
+          status: 'failed',
+          result: result
+        )
       end
     end
   end
